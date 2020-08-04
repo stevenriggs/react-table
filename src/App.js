8a5caa5b9d39da448fbb4490dec15b9928923b01
Steven Riggs <steven.riggs@icloud.com>
2020-08-04 12:02:03 -0400

    sticky table header works!

--- src/App.js
+++ src/App.js
@@ -9,20 +9,55 @@ import {records} from './Data';
 
 class App extends React.Component {
   // {"id":1,"first_name":"Latia","last_name":"Turneaux","email":"lturneaux0@hhs.gov","gender":"Female","ip_address":"197.246.59.178"},
+
   render() {
+    const tableHeight = window.innerHeight + 'px'
+    console.log(tableHeight)
+    const styles = {
+      tablecol: {
+        height: tableHeight,
+        overflow: 'auto'
+      },
+      container: {
+    
+      },
+      right: {
+        height: window.innerHeight, // I want to change this
+        padding: 0,
+        margin: 0,
+        overflow: 'hidden',
+        backgroundColor: 'yellow'
+      },
+      left: {
+        overflowY: '100%',
+        padding: 0,
+        height: window.innerHeight,
+        paddingBottom: '50px',
+        backgroundColor: 'white'
+      },
+      row: {
+        marginBottom: 0
+      }
+    }
+
     return (
       <Container fluid>
         <Row>
           <Col>
-            <Table responsive striped bordered hover size="sm">
+            Hi there
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ height: `calc(100vh - 50px)`, overflow: 'auto' }}>
+            <Table striped bordered hover size="sm">
               <thead>
                 <tr>
-                  <th>ID</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Email</th>
-                  <th>Gender</th>
-                  <th>IP Address</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>ID</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>First Name</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>Last Name</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>Email</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>Gender</th>
+                  <th style={{ background: 'white', position: 'sticky', top: 0 }}>IP Address</th>
                 </tr>
               </thead>
               <tbody>
